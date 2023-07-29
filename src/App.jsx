cb066691e3030e8b67b524ee2913cbd2e276ad5e
faheemk23 <faheemwork23@gmail.com>
2023-07-29 14:13:20 +0530

    feat: side bar

--- src/App.jsx
+++ src/App.jsx
@@ -1,7 +1,24 @@
+import { Route, Routes } from "react-router-dom";
 import "./App.css";
+import SideBar from "./components/SideBar/SideBar";
+import { Explore, Home, PlaylistListing, WatchLater } from "./pages/Pages";
 
 function App() {
-  return <div className="App">mcr 29 july</div>;
+  return (
+    <div className="App">
+      <div className="left">
+        <SideBar />
+      </div>
+      <div className="right">
+        <Routes>
+          <Route path="/" element={<Home />} />
+          <Route path="/explore" element={<Explore />} />
+          <Route path="/playlistlisting" element={<PlaylistListing />} />
+          <Route path="/watchlater" element={<WatchLater />} />
+        </Routes>
+      </div>
+    </div>
+  );
 }
 
 export default App;
